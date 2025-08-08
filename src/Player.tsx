7bff5b3aaa87528382c42a7a6472dca007e6ff8b
Eric <Eric@gmail.com>
2025-08-07 18:27:25 -0600

    Add Jet trail

--- src/Player.tsx
+++ src/Player.tsx
@@ -23,30 +23,37 @@ export function Player({
   const [avatarTexture, setAvatarTexture] = useState(Texture.EMPTY);
   const [baseTexture, setBaseTexture] = useState(Texture.EMPTY);
   const [highlightTexture, setHighlightTexture] = useState(Texture.EMPTY);
-  
+  const [jetTrailTexture, setJetTrailTexture] = useState(Texture.EMPTY);
+  const [baseHighlightTexture, setBaseHighlightTexture] = useState(
+    Texture.EMPTY
+  );
+
   // Animation state
   const [floatOffset, setFloatOffset] = useState(Math.random() * Math.PI * 2);
   const floatSpeed = 0.05; // Speed of the floating animation
   const floatAmplitude = 10; // How far up and down to float
 
   // Animation tick
-  useTick(options => {
-    setFloatOffset((prev) => (prev + floatSpeed * options.deltaTime) % (Math.PI * 2));
+  useTick((options) => {
+    setFloatOffset(
+      (prev) => (prev + floatSpeed * options.deltaTime) % (Math.PI * 2)
+    );
   });
 
-  // Preload the avatar texture
+  // Preload the textures
   useEffect(() => {
-    if (avatarTexture === Texture.EMPTY) {
-      Assets.load(avatar).then((result) => setAvatarTexture(result));
-    }
-  }, [avatarTexture]);
-
-  // Preload the highlight texture
-  useEffect(() => {
-    if (highlightTexture === Texture.EMPTY) {
-      Assets.load("/buzz_in_highlight.png").then((result) => setHighlightTexture(result));
-    }
-  }, [highlightTexture]);
+    Assets.load([
+      avatar,
+      "/buzz_in_highlight.png",
+      "/jet_trail.png",
+      "/player_base_highlight.png",
+    ]).then((result) => {
+      setAvatarTexture(result[avatar]);
+      setHighlightTexture(result["/buzz_in_highlight.png"]);
+      setJetTrailTexture(result["/jet_trail.png"]);
+      setBaseHighlightTexture(result["/player_base_highlight.png"]);
+    });
+  }, []);
 
   // Preload the player base texture
   useEffect(() => {
@@ -56,11 +63,12 @@ export function Player({
   }, [baseTexture]);
 
   return (
-    <pixiContainer 
-      ref={containerRef} 
-      anchor={{ x: 0.5, y: 0.5 }} 
-      x={x} 
-      y={y + Math.sin(floatOffset) * floatAmplitude}>
+    <pixiContainer
+      ref={containerRef}
+      anchor={{ x: 0.5, y: 0.5 }}
+      x={x}
+      y={y + Math.sin(floatOffset) * floatAmplitude}
+    >
       {/* Avatar (top layer) */}
       <pixiSprite
         texture={avatarTexture}
@@ -79,6 +87,16 @@ export function Player({
           scale={scale}
         />
       )}
+      {/* Jet trail */}
+      {isOnline && (
+        <pixiSprite
+          texture={jetTrailTexture}
+          anchor={{ x: 0.5, y: 0.5 }}
+          x={0}
+          y={270 * scale}
+          scale={scale}
+        />
+      )}
       {/* Player base (bottom layer) */}
       <pixiSprite
         texture={baseTexture}
@@ -87,6 +105,16 @@ export function Player({
         y={156 * scale}
         scale={scale}
       />
+      {/* Player base highlight */}
+      {isOnline && (
+        <pixiSprite
+          texture={baseHighlightTexture}
+          anchor={{ x: 0.5, y: 0.5 }}
+          x={0}
+          y={156 * scale}
+          scale={scale}
+        />
+      )}
       {/* Player name text */}
       <pixiText
         text={playerName}
@@ -98,6 +126,7 @@ export function Player({
           fill: 0xffffff,
           stroke: 0x000000,
         }}
+        scale={scale}
       />
       {/* Player score text */}
       <pixiText
@@ -110,6 +139,7 @@ export function Player({
           fill: 0xffffff,
           stroke: 0x000000,
         }}
+        scale={scale}
       />
     </pixiContainer>
   );
